5d2450e1937a5844230c263d980ed3aa414b8e45
jerxmy <jeremy.chambon@mail-esd.com>
2025-03-11 12:26:18 +0100

    correctif

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MainApp from "./App.jsx";
-import "./index.css"; // Assurez-vous que ce fichier existe aussi !
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
